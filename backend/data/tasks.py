2bee119453e619cc7506583195f9737ee8229fe7
vlf0 <dr.dnb2000@gmail.com>
2024-03-27 15:49:49 +0700

    Celery's tasks logic reworked.

--- backend/data/tasks.py
+++ backend/data/tasks.py
@@ -8,7 +8,7 @@ from .models import AccumulationOfIncoming
 
 # Schedule for main logic - inserting data to DMK
 schedule1, _ = CrontabSchedule.objects.get_or_create(
-    minute='45',
+    minute='*',
     hour='*',
     day_of_week='*',
     day_of_month='*',
@@ -18,22 +18,10 @@ schedule1, _ = CrontabSchedule.objects.get_or_create(
 # Schedule for acuumulativing logic - inserting data to DMK
 schedule2, _ = CrontabSchedule.objects.get_or_create(
     minute='0',
-    hour='6',
+    hour='7',
     day_of_week='*',
     day_of_month='1',
     month_of_year='*',
-)
-
-PeriodicTask.objects.get_or_create(
-    crontab=schedule1,
-    name='Saving data to DMK',
-    task='data.tasks.insert_data'
-)
-
-PeriodicTask.objects.get_or_create(
-    crontab=schedule2,
-    name='Removing accumulated data from DMK',
-    task='data.tasks.remove_accum'
 )
 
 
@@ -43,11 +31,12 @@ def insert_data():
     Remove all day caches, process and insert data into DMK.
 
      Update the MainData model with the collected data.
-     Do this everyday at 6:00 AM by schedule.
+     Do this everyday at 7:00 AM by schedule.
     """
-    cache.delete_many(['dmk', 'kis'])
-    ready_data = DataForDMK(KISData(QuerySets().queryset_for_dmk()))
-    ready_data.save_to_dmk()
+    print('main task')
+    # cache.delete_many(['dmk', 'kis'])
+    # ready_data = DataForDMK(KISData(QuerySets().queryset_for_dmk()))
+    # ready_data.save_to_dmk()
 
 
 @shared_task
@@ -55,3 +44,21 @@ def remove_accum():
     AccumulationOfIncoming.objects.truncate_data()
 
 
+tasks_settings = {
+    'collect_task': ('Saving data to DMK', insert_data.name, schedule1),
+    'remove_task': ('Removing accumulated data from DMK', insert_data.name, schedule2),
+}
+
+
+def get_or_create_tasks(tasks_list):
+    for options in tasks_list.values():
+        try:
+            PeriodicTask.objects.get(name=options[0])
+        except:
+            PeriodicTask.objects.create(crontab=options[2],
+                                        name=options[0],
+                                        task=options[1],
+                                        )
+
+
+get_or_create_tasks(tasks_settings)
