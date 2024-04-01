5d7fbb6a51c2541b53e5a2b00c8b4ef62db0648f
vlf0 <dr.dnb2000@gmail.com>
2024-04-01 22:05:14 +0700

    Disable cache controller, added needed dependecie into package.json

--- backend/data/views.py
+++ backend/data/views.py
@@ -7,7 +7,7 @@ from .decorators import main_cache_disable, additional_cache_disable
 
 class KISDataReadViewSet(viewsets.ViewSet):
 
-    @main_cache_disable
+    # @main_cache_disable
     def list(self, request):
         cached_result = Cacher().main_caching()
         return Response(cached_result)
@@ -15,7 +15,7 @@ class KISDataReadViewSet(viewsets.ViewSet):
 
 class KISDataAnotherDates(viewsets.ViewSet):
 
-    @additional_cache_disable
+    # @additional_cache_disable
     def list(self, request):
         cached_result = Cacher.additional_caching(request)
         return Response(cached_result)
